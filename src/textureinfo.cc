679be354fd07f77e2d9a802afb623ae38e9e04ff
Khruschev Dmitry aka DimaO <hruschev_da@romz.ru>
2025-12-18 14:48:34 +0300

    Basics GLTFTextureInfo class were complete

---
+++ src/textureinfo.cc
@@ -0,0 +1,81 @@
+#include "textureinfo.h"
+#include "gltfobject.h"
+
+#include <cstddef>
+#include <iostream>
+
+namespace gltfloader
+{
+
+/* ******************** GLTFTextureInfo::GLTFTextureInfo ******************* */
+
+GLTFTextureInfo::GLTFTextureInfo () : GLTFObject ()
+{
+  //
+}
+
+/* ******************* GLTFTextureInfo::~GLTFTextureInfo ******************* */
+
+GLTFTextureInfo::~GLTFTextureInfo ()
+{
+  //
+}
+
+/* ************************* GLTFTextureInfo::index ************************ */
+
+size_t
+GLTFTextureInfo::index () const
+{
+  return m_index;
+}
+
+/* *********************** GLTFTextureInfo::tex_coord ********************** */
+
+size_t
+GLTFTextureInfo::tex_coord () const
+{
+  return m_tex_coord;
+}
+
+/* ************************ GLTFTextureInfo::create ************************ */
+
+std::shared_ptr<GLTFTextureInfo>
+GLTFTextureInfo::create (const IndexHelper &helper, int index,
+                         const std::optional<int> &tex_coord)
+{
+  std::shared_ptr<GLTFTextureInfo> tmp (new GLTFTextureInfo ());
+
+  // Check index boundaries
+  if (index < 0)
+    {
+      std::cout << "[W] glTF 2.0 5.30.1 index can not be negative"
+                << std::endl;
+      return nullptr;
+    }
+
+  size_t shifted_index = index + helper.textures_size ();
+  if (shifted_index >= helper.textures_size ())
+    {
+      std::cout << "[W] glTF 2.0 5.30.1 index is out of range" << std::endl;
+      return nullptr;
+    }
+
+  tmp->m_index = shifted_index;
+
+  // Check texCoord value
+  if (tex_coord.has_value ())
+    {
+      if (tex_coord.value () < 0)
+        {
+          std::cout << "[W] glTF 2.0 5.30.2 texCoord can not be negative"
+                    << std::endl;
+          return nullptr;
+        }
+    }
+  tmp->m_tex_coord = tex_coord.value_or (0);
+
+  // NOTE : Check for texture coordinates index in the mesh object
+  return tmp;
+}
+
+}
---
+++ src/textureinfo.h
@@ -0,0 +1,33 @@
+#ifndef TEXTUREINFO_H
+#define TEXTUREINFO_H
+
+#include "gltfobject.h"
+#include "indexhelper.h"
+
+#include <cstddef>
+#include <memory>
+#include <optional>
+
+namespace gltfloader
+{
+
+class GLTFTextureInfo : public GLTFObject
+{
+private:
+  size_t m_index;
+  size_t m_tex_coord;
+  GLTFTextureInfo ();
+
+public:
+  virtual ~GLTFTextureInfo ();
+  size_t index () const;
+  size_t tex_coord () const;
+
+  static std::shared_ptr<GLTFTextureInfo>
+  create (const IndexHelper &helper, int index,
+          const std::optional<int> &tex_coord);
+};
+
+}
+
+#endif // TEXTUREINFO_H
