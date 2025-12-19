9eee59e4f38ea2d58f7aa2705c68b7c5922193c0
Khruschev Dmitry aka DimaO <hruschev_da@romz.ru>
2025-12-19 08:54:59 +0300

    GLTFTexture

--- src/texture.cc
+++ src/texture.cc
@@ -4,6 +4,10 @@
 #include <iostream>
 #include <optional>
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif // HAS_CONFIG_H
+
 namespace gltfloader
 {
 
@@ -38,23 +42,38 @@ GLTFTexture::create (const IndexHelper &helper, const std::string &name,
 {
   std::shared_ptr<GLTFTexture> tmp (new GLTFTexture (name));
 
+  size_t shifted_sampler_index;
+
+  // NOTE : We are do not use default sampler from default samplers storage in
+  // IndexHelper. Instead we just save std::nullopt to the appropriate field
+  // and wait for upper layer to fill it instead.
   if (sampler == std::nullopt)
     {
-      tmp->m_sampler = helper.default_texture_sampler_index();
-    }
-  else if (sampler < 0)
-    {
-      std::cout << "[W] glTF 2.0 5.29.1: texture.sampler >= 0" << std::endl;
-      return nullptr;
-    }
-  else if (sampler >= helper.samplers_size ())
-    {
       tmp->m_sampler = sampler;
+    }
+  else
+    {
+      shifted_sampler_index
+          = sampler.value () + helper.sampler_defaults_size ();
+      if (sampler < 0)
+        {
+          std::cout << "[W] glTF 2.0 5.29.1: texture.sampler >= 0"
+                    << std::endl;
+          return nullptr;
+        }
+      else if (sampler >= helper.samplers_size ())
+        {
+          tmp->m_sampler = sampler;
+        }
     }
 
   if (source == std::nullopt)
     {
       // TODO : Special mechanism from extensions (glTF 2.0 5.29.2)
+      std::cout << "[W] glTF 2.0 5.29.2: missing texture.source is valid but "
+                   "not supported by "
+                << PACKAGE << " " << VERSION << std::endl;
+      return nullptr;
     }
   else if (source < 0)
     {
